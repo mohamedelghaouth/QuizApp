eb3d6260827dc81ba4626a7ca9c75787158514a3
mohamedelghaouth <44144437+mohamedelghaouth@users.noreply.github.com>
2021-01-03 04:23:30 +0100

    create ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {ListGroup} from 'react-bootstrap';
 
 function App() {
+
+  const [selected, setselected] = useState(0);
+//test
+  const [answer, setanswer] = useState(0);
+
+ 
+  const [userAnswered, setuserAnswered] = useState(false);
+
+
+  const changecolor=(position)=>{
+     if(userAnswered){
+      if(selected === position)
+      {
+        if(selected === answer)   return 'green';
+        return 'red';
+      }
+      if(answer === position)   return 'green';
+     }
+            
+      return ' ';
+  };
+  const changeselected=(position)=>{
+    setuserAnswered(true);
+    setselected(position);
+};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Question</h1> 
       </header>
+      <body className="App-body"> 
+        <ListGroup id="ListGroup">
+          <ListGroup.Item className={"Item  " + changecolor(0) }  onClick={()=>changeselected(0)}> answezrgthysrtujhszryjsyjr</ListGroup.Item>
+          <ListGroup.Item className={"Item " + changecolor(1) }  onClick={()=>changeselected(1)}>answer</ListGroup.Item>
+          <ListGroup.Item className={"Item " + changecolor(2) }  onClick={()=>changeselected(2)}>anssjyrjyjwer</ListGroup.Item>
+          <ListGroup.Item className={"Item " + changecolor(3) }  onClick={()=>changeselected(3)}> anssrtjhsrtjryjeztykztuykzuèkizeswer</ListGroup.Item>
+          {userAnswered? (<ListGroup.Item disabled className='next'>next</ListGroup.Item>):null}
+        </ListGroup>
+      </body>
     </div>
   );
 }
